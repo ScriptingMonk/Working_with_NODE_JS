bf697a8a2c7066d9b867c7b96658fb3c3482e8e7
ScriptingMonk <imadityaprasad30@gmail.com>
2023-09-20 22:35:06 +0530

    second commit

--- 7-Async-Async_Await-2.js
+++ 7-Async-Async_Await-2.js
@@ -1,6 +1,10 @@
+/*  Async : Without writing our own getText function */
+
+/*
 const {readFile , writeFile, read } = require('fs')
 
-const util = require('util')
+//   Using utility built-in module  
+const util = require('util') 
 const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
 
@@ -18,3 +22,24 @@ const start = async()=>{
 }
 
 start()
+*/
+
+/*  Async : Without using util module too */
+
+
+const {readFile , writeFile } = require('fs').promises
+
+const startA = async()=>{
+    try {
+        const first = await readFile('C:/Users/iadit/OneDrive/Desktop/waste/CODING/WEB DEV/NODE_JS/3-Built_in_Path_Module_Content/First.txt','utf-8')
+        const second = await readFile('C:/Users/iadit/OneDrive/Desktop/waste/CODING/WEB DEV/NODE_JS/3-Built_in_Path_Module_Content/Second.txt','utf-8')
+        console.log(first , second);
+    
+        const write = await writeFile('C:/Users/iadit/OneDrive/Desktop/waste/CODING/WEB DEV/NODE_JS/3-Built_in_Path_Module_Content/Async_ResultA.txt',`THE RESULT IS : ${first} ${second}`)
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startA()
